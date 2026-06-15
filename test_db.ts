6d490a02e2a17929c73ffbf367e768633cdeddf8
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2026-06-15 16:28:37 +0700

    chore: remove unnecessary files

--- test_db.ts
+++
@@ -1,8 +0,0 @@
-import { db } from '@xbrk/db/client';
-import { articles } from '@xbrk/db/schema';
-
-async function main() {
-  const allArticles = await db.select().from(articles);
-  console.log(allArticles);
-}
-main().catch(console.error);
