263d6302d89f97d1c90d46b421c2b80f3966d249
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2026-06-15 23:22:35 +0700

    feat(db): add contentRendering columns to project/snippet/service and Drizzle migration

--- packages/db/drizzle/0000_curved_ravenous.sql
+++ packages/db/drizzle/0000_curved_ravenous.sql
@@ -18,6 +18,8 @@ CREATE TABLE "articles" (
 	"slug" varchar(255) NOT NULL,
 	"description" varchar(255),
 	"content" text,
+	"content_rendering" text,
+	"content_rendering_version" integer DEFAULT 1 NOT NULL,
 	"image_url" varchar(255),
 	"is_draft" boolean DEFAULT false NOT NULL,
 	"tags" text[],
