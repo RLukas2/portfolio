979ad9a2c910db4c9f2d76790908f3048f5aeffd
Ngô Hoàng Tuấn <rickielukas@gmail.com>
2026-06-18 19:07:18 +0700

    fix(md): correct RENDERING_CONFIG_ID, remove unused export

--- packages/md/src/processor/version.ts
+++ packages/md/src/processor/version.ts
@@ -1,4 +1,3 @@
 export const RENDERING_VERSION = 9;
 
-export const RENDERING_CONFIG_ID =
-  'remark-gfm+math+emoji+sanitize-no-clobber+footnotes+rehype-katex+slug+autolink+shiki-v1';
+export const RENDERING_CONFIG_ID = 'remark-gfm+math+emoji+sanitize-no-clobber+rehype-katex+slug+autolink+shiki-v1';
